f610a72535ad13db767c74bdc3c24d29f6a16fdb
maqwjz.scripts <egyptzamses@gmail.com>
2025-09-13 19:43:35 +0400

    Delete ngrok.sh

--- ngrok.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-echo "📥 Installing Ngrok..."
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -O ngrok.zip
-unzip ngrok.zip
-chmod +x ngrok
-rm ngrok.zip
-echo "✅ Ngrok installed!"
